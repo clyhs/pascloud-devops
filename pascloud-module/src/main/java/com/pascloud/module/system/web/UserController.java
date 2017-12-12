d714668c30f98303155fc6699dc6065b5f19968f
clygd <clygd@126.com>
2017-12-12 14:33:03 +0800

    test

--- pascloud-module/src/main/java/com/pascloud/module/system/web/UserController.java
+++ pascloud-module/src/main/java/com/pascloud/module/system/web/UserController.java
@@ -3,6 +3,7 @@ package com.pascloud.module.system.web;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
---
+++ pascloud-module/src/test/java/com/pascloud/module/user/UserTestController.java
@@ -0,0 +1,67 @@
+package com.pascloud.module.user;
+
+import java.util.List;
+
+import javax.annotation.Resource;
+import javax.inject.Inject;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import com.pascloud.bean.system.User;
+import com.pascloud.dao.system.UserDao;
+import com.pascloud.module.system.web.UserController;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+@ContextConfiguration(locations = { "classpath*:META-INF/spring/pascloud-*.xml" })
+public class UserTestController   {
+
+	@Autowired
+	private WebApplicationContext wac;
+
+	@Autowired
+	private UserDao userDao;// 你要测试的Controller
+	@Autowired
+	private UserController userController;
+
+	private MockMvc mockMvc;
+	
+	@Before  
+    public void setup() {  
+        mockMvc = MockMvcBuilders.standaloneSetup(userController).build();  
+    }  
+  
+    @Test  
+    public void testFindPageUsers() throws Exception {  
+    	/*
+        ResultActions ra = this.mockMvc.perform(MockMvcRequestBuilders  
+                .get("/module/user/getusers")  
+                .accept(MediaType.APPLICATION_JSON).param("page", "1")  
+                .param("limit", "10"));  */
+    	
+    	ResultActions ra = this.mockMvc.perform(MockMvcRequestBuilders  
+                .get("/module/user/getusers")  
+                .accept(MediaType.APPLICATION_JSON));
+        MvcResult mr = ra.andReturn();  
+        String result = mr.getResponse().getContentAsString();  
+        System.out.println(result); 
+    	/*
+    	List<User> users = userDao.selectall();
+    	System.out.println(users.size());*/
+    } 
+
+}
