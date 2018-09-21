a606ac1e964741c89f4bdd881afebedc1ecb508d
clygd <clygd@126.com>
2018-09-21 16:35:48 +0800

    resources

--- pascloud-core/src/main/java/com/pascloud/core/script/ScriptPool.java
+++ pascloud-core/src/main/java/com/pascloud/core/script/ScriptPool.java
@@ -1,7 +1,13 @@
 package com.pascloud.core.script;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -47,7 +53,140 @@ public final class ScriptPool {
 		//log.info("", source, out, jarsDir);
 	}
 	
+	@SuppressWarnings("unchecked")
+	public <T extends IIdScript> T getIIdScript(Integer modelID) {
+		IIdScript iis = null;
+		if (idScriptInstances.containsKey(modelID)) {
+			iis = idScriptInstances.get(modelID);
+		}
+		return (T) iis;
+	}
+	
+	/**
+	 * 脚本列表
+	 * @param name
+	 * @return
+	 */
+	@SuppressWarnings("unchecked")
+	public <E> Collection<E> getEvts(String name) {
+		Map<String, IScript> scripts = ScriptPool.this.scriptInstances.get(name);
+		if (scripts != null) {
+			return (Collection<E>) scripts.values();
+		}
+		return new ArrayList<>();
+	}
+	
+	/**
+	 * 脚本列表
+	 * @param clazz
+	 * @return
+	 */
+	@SuppressWarnings("unchecked")
+	public <E> Collection<E> getEvts(Class<E> clazz) {
+		Map<String, IScript> scripts = ScriptPool.this.scriptInstances.get(clazz.getName());
+		if (scripts != null) {
+			return (Collection<E>) scripts.values();
+		}
+		return new ArrayList<>();
+	}
+	
+	/**
+	 * 执行脚本
+	 * @param scriptClass
+	 * @param action
+	 */
+	@SuppressWarnings("unchecked")
+	public <T extends IScript> void executeScripts(Class<T> scriptClass, Consumer<T> action) {
+		Collection<IScript> evts = getEvts(scriptClass.getName());
+		if (evts != null && !evts.isEmpty() && action != null) {
+			evts.forEach(scrpit -> {
+				try {
+					action.accept((T) scrpit);
+				} catch (Exception e) {
+					log.error("执行 IScript:" + scriptClass.getName(), e);
+				}
+			});
+		}
+	}
+	
+	
+	/**
+	 * 执行脚本，当执行结果为true时，中断执行，并返回true。否则统一返回执行false		
+	 * @param scriptClass
+	 * @param condition
+	 * @return
+	 */
+	@SuppressWarnings("unchecked")
+	public <T extends IScript> boolean predicateScripts(Class<? extends IScript> scriptClass, Predicate<T> condition) {
+		Collection<IScript> evts = getEvts(scriptClass.getName());
+		if (evts != null && !evts.isEmpty() && condition != null) {
+			Iterator<IScript> iterator = evts.iterator();
+			while (iterator.hasNext()) {
+				try {
+					if (condition.test((T) iterator.next())) {
+						return true;
+					}
+				} catch (Exception e) {
+					log.error("predicateScripts IScript:" + scriptClass.getName(), e);
+				}
+			}
+		}
+		return false;
+	}
+	
+	/**
+	 * 执行脚本，并返回一个值
+	 * @param scriptClass
+	 * @param function
+	 * @return
+	 */
+	@SuppressWarnings("unchecked")
+	public <T extends IScript, R> R functionScripts(Class<? extends IScript> scriptClass, Function<T, R> function) {
+		Collection<IScript> evts = getEvts(scriptClass.getName());
+		if (evts != null && !evts.isEmpty() && function != null) {
+			Iterator<IScript> iterator = evts.iterator();
+			while (iterator.hasNext()) {
+				try {
+					R r = function.apply((T) iterator.next());
+					if (r != null) {
+						return r;
+					}
+				} catch (Exception e) {
+					log.error("functionScripts IBaseScript:" + scriptClass.getName(), e);
+				}
+			}
+		}
+		return null;
+	}
+	
+	
 	final boolean stringIsNullEmpty(String str) {
 		return str == null || str.length() <= 0 || "".equals(str.trim());
 	}
+
+	public Map<String, Map<String, IScript>> getTmpScriptInstances() {
+		return tmpScriptInstances;
+	}
+
+	public Map<Integer, IIdScript> getTmpIdScriptInstances() {
+		return tmpIdScriptInstances;
+	}
+
+	public Map<Integer, Class<? extends IHandler>> getTcpHandlerMap() {
+		return tcpHandlerMap;
+	}
+
+	public Map<Integer, HandlerEntity> getTcpHandlerEntityMap() {
+		return tcpHandlerEntityMap;
+	}
+
+	public Map<String, Class<? extends IHandler>> getHttpHandlerMap() {
+		return httpHandlerMap;
+	}
+
+	public Map<String, HandlerEntity> getHttpHandlerEntityMap() {
+		return httpHandlerEntityMap;
+	}
+	
+	
 }
