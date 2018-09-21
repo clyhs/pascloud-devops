d0a358a6606fca38f9b34b8af94021ef1cbbbc59
clygd <clygd@126.com>
2018-09-21 10:53:12 +0800

    message

---
+++ pascloud-core/src/main/java/com/pascloud/core/mina/message/MassMessage.java
@@ -0,0 +1,41 @@
+package com.pascloud.core.mina.message;
+
+import java.util.Collection;
+import org.apache.mina.core.buffer.IoBuffer;
+
+/**
+ * 群发消息
+ */
+public class MassMessage {
+
+    private final IoBuffer buffer;
+    private final Collection<Long> targets;	//用户ID
+
+    public MassMessage(IoBuffer buffer, Collection<Long> targets) {
+        this.buffer = buffer;
+        this.targets = targets;
+        this.buffer.rewind();
+    }
+
+    public int getLength() {
+        return buffer.remaining() + targets.size() * 8;
+    }
+    
+    public int getBuffLength() {
+        return buffer.remaining();
+    }
+
+    /**
+     * @return the buffer
+     */
+    public IoBuffer getBuffer() {
+        return buffer;
+    }
+
+    /**
+     * @return the targets
+     */
+    public Collection<Long> getTargets() {
+        return targets;
+    }
+}
