b4a8e6154dd845ab415ccddbedfe55dc8a428a35
clygd <clygd@126.com>
2018-09-26 14:31:54 +0800

    script

--- pascloud-webapps/src/main/webapp/static/resources/script/oracle/script/imp_dmpV2.sh
+++ pascloud-webapps/src/main/webapp/static/resources/script/oracle/script/imp_dmpV2.sh
@@ -2,7 +2,8 @@
 ##su - oracle <<EON
 export ORACLE_SID=$1
 export ORACLE_HOME=$2
-$ORACLE_HOME/bin/imp $3/$4@$ORACLE_SID file=/home/oracle/script/cloudpas_ddl.dmp fromuser=PAS touser=$3 ignore=y
-$ORACLE_HOME/bin/imp $3/$4@$ORACLE_SID file=/home/oracle/script/data.dmp fromuser=PAS touser=$3 ignore=y
+export NLS_LANG="AMERICAN_AMERICA.ZHS16GBK"
+$ORACLE_HOME/bin/imp $3/$4@$ORACLE_SID file=/home/oracle/script/cloudpas_ddl.dmp fromuser=PAS touser=$3 ignore=y buffer=10485760
+$ORACLE_HOME/bin/imp $3/$4@$ORACLE_SID file=/home/oracle/script/data.dmp fromuser=PAS touser=$3 ignore=y buffer=10485760
 ##exit
 ##EON
