7d8021cdf2a7f8f6360c03784f5c1087c6a56fa3
clyhs <clygd@126.com>
2018-09-29 22:07:23 +0800

    g

---
+++ pascloud-cluster/src/main/java/com/pascloud/cluster/App.java
@@ -0,0 +1,13 @@
+package com.pascloud.cluster;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
