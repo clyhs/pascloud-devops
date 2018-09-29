7336b6b97f62700783a691377894662792a90099
clyhs <clygd@126.com>
2018-09-29 22:40:21 +0800

    game

---
+++ pascloud-game/pascloud-game-model/src/main/java/com/pascloud/game/model/App.java
@@ -0,0 +1,13 @@
+package com.pascloud.game.model;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
