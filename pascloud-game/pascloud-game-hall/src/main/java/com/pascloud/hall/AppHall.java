c064b2ea173d49dc3c2342a4055109cc602c40df
clyhs <clygd@126.com>
2018-10-16 11:02:10 +0800

    hall

---
+++ pascloud-game/pascloud-game-hall/src/main/java/com/pascloud/hall/AppHall.java
@@ -0,0 +1,13 @@
+package com.pascloud.hall;
+
+/**
+ * Hello world!
+ *
+ */
+public class AppHall 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
