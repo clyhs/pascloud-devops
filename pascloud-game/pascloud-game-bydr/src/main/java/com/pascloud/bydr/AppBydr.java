93177a64ab6705d364b06746b42ed81e10751020
clyhs <clygd@126.com>
2018-10-16 17:02:35 +0800

    bydr

---
+++ pascloud-game/pascloud-game-bydr/src/main/java/com/pascloud/bydr/AppBydr.java
@@ -0,0 +1,13 @@
+package com.pascloud.bydr;
+
+/**
+ * Hello world!
+ *
+ */
+public class AppBydr 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
