fe9458592dc0d6d1ea1431a47e8745a983a3ac2a
clyhs <clygd@126.com>
2018-10-19 09:23:52 +0800

    bat

---
+++ pascloud-game/pascloud-game-bydr-world/start.bat
@@ -0,0 +1,4 @@
+title cluster
+java -jar -server -Xms512M -Xmx512M target/pascloud-game-bydr-world-0.0.1-SNAPSHOT.jar
+
+pause
---
+++ pascloud-game/pascloud-game-cluster/start.bat
@@ -0,0 +1,4 @@
+title cluster
+java -jar -server -Xms512M -Xmx512M target/pascloud-game-cluster-0.0.1-SNAPSHOT.jar
+
+pause
